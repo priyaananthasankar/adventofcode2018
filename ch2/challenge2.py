00095541d32fcf9cefb27f69e1a4e97e3a7d7df8
Priya Ananthasankar <prananth@microsoft.com>
2019-01-01 23:32:07 -0800

    Checksum commit

--- ch2/challenge2.py
+++ ch2/challenge2.py
@@ -1,25 +1,26 @@
-# Part 1
+import collections
 with open("input.txt") as f:
-    input = [int(x.strip()) for x in f.readlines()]
-    final_answer = 0
-    for i in input:
-        final_answer += i
-    print(final_answer)     
+    input = [x.strip() for x in f.readlines()]
+    two_list = 0
+    three_list = 0
+    for item in input:
+        d = collections.defaultdict(int)
+        for c in item:
+            d[c] += 1
+        two_count_per_item = 0 
+        three_count_per_item = 0
+        for c in d:
+            if d[c] == 2:
+                two_count_per_item += 1
+            if d[c] == 3:
+                three_count_per_item += 1
 
-# Part 2
-with open("input.txt") as f:
-    input = [int(x.strip()) for x in f.readlines()]
-    final_answer = 0
-    freq = {}
-    repeat = True
-    while repeat:
-        print("Going once...")
-        for i in input:
-            final_answer += i
-            if final_answer in freq:
-                print(final_answer)
-                repeat = False
-                break
-            else:
-                freq[final_answer] = 1
+        # count occurances just once
+        if two_count_per_item >= 1:
+            two_list +=1
+        if three_count_per_item >= 1:
+            three_list += 1
+
+# checksum
+print(two_list * three_list)
     
